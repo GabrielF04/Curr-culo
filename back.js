5a06e73f648bed9f417d5bce962615efc9738258
GabrielF04 <122302554+GabrielF04@users.noreply.github.com>
2023-01-25 15:33:25 -0300

    Add files via upload

--- back.js
+++ back.js
@@ -8,16 +8,17 @@ class MobileNavbar {
         this.handleClick = this.handleClick.bind();
     }
     animatedLinks() {
-        this.navLinks.forEach((link)=> {
+        this.navLinks.forEach((link, index)=> {
           link.style.animation
           ? (link.style.animation = "")
-          : (link.style.animation = `navLinkFade 0.5s ease forwards 0.
-          3s`)
+          : (link.style.animation = `navLinkFade 0.5s ease forwards $ 
+          {index / 7 + 0.3}s`);
         });
     }
 
     handleClick() {
         this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
         this.animatedLinks();
     }
 
@@ -37,3 +38,20 @@ const MobileNavBar = new MobileNavbar(
     ".nav-list",
     ".nav-list li",
 )
+
+const allLinks = document.querySelectorAll("nav-list");
+
+for(const link of allLinks){
+    link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e){
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
+    console.log(href)
+    scroll({
+        top: offsetTop,
+        behavior: "smooth"
+    })
+}
